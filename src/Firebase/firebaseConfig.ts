5f405724bc9575a54227affdf7cd6d4a7d1f7d4f
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-07-27 19:59:00 +0530

    refactor: loading issues fxing

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApqtM077r9VoVwg928slO9LKtLR0Wk_ys",
+  authDomain: "tedxccet.firebaseapp.com",
+  projectId: "tedxccet",
+  storageBucket: "tedxccet.appspot.com",
+  messagingSenderId: "653705804844",
+  appId: "1:653705804844:web:03fca4a4e302271dd0a589",
+  measurementId: "G-6X312LT9J2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/common/components/X.tsx
+++ src/common/components/X.tsx
@@ -7,6 +7,6 @@ interface Xprops{
 
 export default function X(props:Xprops) {
   return (
-    <Image src={"/xlogo.png"} height={380} width={380} alt='' className={props?.className}/>
+    <Image src={"https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2Fxlogo.png?alt=media&token=909c4bc2-d2da-43eb-addc-fee9a1501a53"} height={380} width={380} alt='' className={props?.className}/>
   )
 }
