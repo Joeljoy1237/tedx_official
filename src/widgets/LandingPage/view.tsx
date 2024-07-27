41bd20abb142862fc87aa494677248e2e3a79a0c
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-07-27 16:01:17 +0530

    feat: loading issues

--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -1,12 +1,31 @@
-import Video from "@core/Video";
-import React from "react";
+"use client"
+
+// import Video from "@core/Video";
+import React, { useEffect } from "react";
 import HeroText from "@widgets/LandingPage/components/HeroText";
 import AboutView from "@widgets/About";
 import Welcome from "@widgets/LandingPage/components/Welcome";
-import ScrollTextView from "@widgets/LandingPage/components/ScrollTextView";
+// import ScrollTextView from "@widgets/LandingPage/components/ScrollTextView";
 import OurThemeView from "@widgets/OurTheme";
+import dynamic from "next/dynamic";
+
+const ScrollTextView = dynamic(() => import("@widgets/LandingPage/components/ScrollTextView"), {
+  ssr: false,
+});
+
+const Video = dynamic(() => import("@core/Video"), {
+  ssr: false,
+});
 
 export default function LandingPageView() {
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
   return (
     <>
       <div className="bg-slk-black-200 relative overflow-hidden w-full min-h-screen pt-10">
