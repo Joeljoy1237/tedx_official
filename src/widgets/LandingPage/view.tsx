eec6cd0339a7d1b623a9922b16277b24f5bc0efc
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-07-28 09:23:52 +0530

    refactor

--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -10,7 +10,7 @@ export default function LandingPageView() {
   return (
     <>
       <div className="bg-slk-black-200 relative overflow-hidden w-full min-h-screen">
-        <Video url="home2.mp4" className="absolute inset-0 w-full h-full object-cover" />
+        {/* <Video url="/home.gif" className="absolute inset-0 w-full h-full object-cover" /> */}
         <div className="absolute inset-0 bg-black-100 opacity-80" />
         <HeroText />
       </div>
