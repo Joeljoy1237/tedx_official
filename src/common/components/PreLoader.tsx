cb5b4d86ed81a344be46b8f3424bce19c176273d
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-07-28 14:56:35 +0530

    feat: loader

--- src/common/components/PreLoader.tsx
+++ src/common/components/PreLoader.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 export default function PreLoader() {
   return (
-    <div className='fixed inset-0 z-[1000] bg-black-100 flex items-center justify-center'>
-      <div className="text-white">Loading...</div>
+    <div className="fixed inset-0 z-[1000] bg-black-100 flex items-center justify-center">
+      <div className="text-white">
+        <div className="flex items-center justify-center flex-col gap-4">
+          <Image src={"/Logo.svg"} height={300} width={340} alt="" />
+          <span className="loader"></span>
+        </div>
+      </div>
     </div>
   );
 }
