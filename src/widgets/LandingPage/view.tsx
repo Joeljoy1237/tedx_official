b0ad21c462a55bf1389fe18e644b4b5a1530107b
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-07-28 10:20:49 +0530

    refactor: bg-video

--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -11,7 +11,7 @@ export default function LandingPageView() {
   return (
     <>
       <div className="bg-slk-black-200 relative overflow-hidden w-full min-h-screen py-14">
-        <Video url="https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2FWhatsApp%20Video%202024-07-27%20at%2020.webm?alt=media&token=90e5f7b0-cb06-4ac4-9129-7f75e6a61336" className="absolute inset-0 w-full h-full object-cover" />
+        <Video url="https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2FWhatsApp%20Video%202024-07-27%20at%2016.09.00.mp4?alt=media&token=b0813adc-5864-4830-9830-454be63a8ec6" className="absolute inset-0 w-full h-full object-cover" />
         {/* <div className="circlePosition bg-primary-700 h-[15rem] items-center justify-center flex w-[15rem] absolute top-[50%] left-[10%] translate-x-[-50%] translate-y-[-50%] blur-[130px]"></div>
         <div className="circlePosition bg-primary-700 h-[10rem] w-[10rem] absolute top-[50%] left-[70%] translate-x-[-50%] translate-y-[-50%] blur-[110px]"></div> */}
         {/* <div className="circlePosition bg-primary-700 bg-opacity-50 h-[15rem] w-[15rem] absolute top-[10%] left-[93%] translate-x-[-50%] translate-y-[-50%] blur-[90px]"></div> */}
