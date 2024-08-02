9d5a58926e2cf8ed9cfa3f6880ddf33a0811cf4b
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-08-02 07:40:58 +0530

    feat

--- src/widgets/LandingPage/components/BannerView.tsx
+++ src/widgets/LandingPage/components/BannerView.tsx
@@ -1,55 +1,30 @@
-"use client"
+"use client";
 import React, { Suspense, useEffect, useRef } from "react";
 import HeroText from "./HeroText";
-
-type VideoProps = {
-  url: string;
-  className?: string;
-};
-
-function Video({ url, className }: VideoProps) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    const videoElement = videoRef.current;
-
-    if (videoElement) {
-      const handleCanPlay = () => {
-        videoElement.play().catch((error) => {
-          console.error("Video play error:", error);
-        });
-      };
-
-      videoElement.addEventListener("canplay", handleCanPlay);
-
-      return () => {
-        videoElement.removeEventListener("canplay", handleCanPlay);
-      };
-    }
-  }, []);
-
-  return (
-    <video
-      ref={videoRef}
-      className={className}
-      src={url}
-      playsInline
-      muted
-      autoPlay
-      loop
-      preload="auto"
-    />
-  );
-}
 
 export default function BannerView() {
   return (
     <div className="bg-slk-black-200 flex flex-col relative overflow-hidden w-full h-[100vh] md:min-h-screen lg:min-h-screen">
       <Suspense fallback={<>Loading...</>}>
-        <Video
-          url="https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2Flow_bit%20(1).mp4?alt=media&token=517c6e35-d681-4018-ba79-310d29b89c73"
+        <video
+          width="320"
+          height="240"
+          controls={false}
+          preload="none"
+          autoPlay
+          muted
+          loop
+          playsInline
           className="absolute inset-0 w-full h-full object-cover flex md:flex lg:flex"
-        />
+        >
+          <source
+            src={
+              "https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2Flow_bit%20(1).mp4?alt=media&token=517c6e35-d681-4018-ba79-310d29b89c73"
+            }
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       </Suspense>
       <div className="absolute inset-0 bg-black-100 opacity-70 md:opacity-70 lg:opacity-70" />
       <HeroText />
--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -1,20 +1,55 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 import AboutView from "@widgets/About";
 import Welcome from "@widgets/LandingPage/components/Welcome";
 import OurThemeView from "@widgets/OurTheme";
 import PreLoader from "@components/PreLoader";
 import HeaderView from "@widgets/Header";
 import BannerView from "./components/BannerView";
-import dynamic from "next/dynamic";
 import FooterView from "@widgets/Footer";
 import AboutSection from "./components/AboutSection";
 import ThemeSection from "./components/Theme";
 import Lenis from "lenis";
 
+// Dynamically import the SmoothScrollCustom component without SSR
+const SmoothScrollCustom = dynamic(
+  () => import("@components/ScrollSmoother"),
+  {
+    ssr: false,
+  }
+);
+
+// Dynamically import the ScrollTextView component without SSR
+const ScrollTextView = dynamic(
+  () => import("@widgets/LandingPage/components/ScrollTextView"),
+  {
+    ssr: false,
+  }
+);
+
 const LandingPageView: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
 
+  // Check if the window object is available and set the isDesktop state
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    // Set initial state
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Set a timeout to simulate loading
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoaded(true);
@@ -22,19 +57,6 @@ const LandingPageView: React.FC = () => {
 
     return () => clearTimeout(timer);
   }, []);
-
-  const ScrollTextView = dynamic(
-    () => import("@widgets/LandingPage/components/ScrollTextView"),
-    {
-      ssr: false,
-    }
-  );
-  const SmoothScrollCustom = dynamic(
-    () => import("@components/ScrollSmoother"),
-    {
-      ssr: false,
-    }
-  );
 
   return (
     <div className="w-full flex flex-col gap-2">
@@ -46,7 +68,7 @@ const LandingPageView: React.FC = () => {
       <ThemeSection />
       <AboutSection />
       <FooterView />
-      <SmoothScrollCustom />
+      {isDesktop && <SmoothScrollCustom />}
     </div>
   );
 };
