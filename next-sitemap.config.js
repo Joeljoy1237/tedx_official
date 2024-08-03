0fb971071d9f02c90f36fda14e38d83910757f0b
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-08-03 20:27:00 +0530

    feat: seo setting

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.tedxccet.in',
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+};
--- next-sitemap.config.ts
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.tedxccet.in',
-    generateRobotsTxt: true,
-  };
-  
