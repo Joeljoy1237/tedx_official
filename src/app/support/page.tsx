49b6f08dffeadff8872732eb33b099b6c7911921
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-08-06 07:10:08 +0530

    suport

---
+++ src/app/support/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import SuportView from '@widgets/Support'
+
+export default function page() {
+  return (
+    <SuportView/>
+  )
+}
---
+++ src/widgets/Support/index.ts
@@ -0,0 +1,3 @@
+import SuportView from "./view";
+
+export default SuportView
