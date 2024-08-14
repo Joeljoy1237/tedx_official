1e0544f6fcadab95e5770ba576ec04a9c2045a46
Joeljoy1237 <joeljoy1237@gmail.com>
2024-08-14 20:12:27 +0530

    fix: conflit resolve

--- src/app/api/payment/check/route.ts
+++ src/app/api/payment/check/route.ts
@@ -2,7 +2,8 @@ import Booking from "@models/Booking";
 import { connectToDB } from "@utils/database";
 import Razorpay from "razorpay";
 import nodemailer from "nodemailer";
-import { NextRequest, NextResponse } from "next/server"; // Assuming  're using Next.js
+import { NextRequest, NextResponse } from "next/server";
+import Ticket from "@models/Ticket";
 
 interface GroupMember {
   firstName: string;
@@ -37,6 +38,7 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
       key_secret: process.env.RAZOR_KEY_SECRET as string,
     });
 
+
     const paymentData = await razorpay.orders.fetch(orderId);
 
     if (
@@ -51,7 +53,7 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
         groupMember.ticketId = `TEDXCCET/2024/${("00" + ticket.ticketSold).slice(-3)}`
 
       })
-      console.log(group)
+
       const amount = paymentData.amount_paid;
       const newBooking = new Booking({
         userId,
@@ -64,82 +66,13 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
       await newBooking.save();
       await ticket.save();
 
-      for (const data of group) {
-        // Generate PDF
+      group.forEach(async (data) => {
         const mailOptions = {
           from: process.env.EMAIL,
           to: data.email,
-          subject: "Your TEDxCCET Ticket Confirmation",
-          html: `
-            <html>
-            <head>
-              <style>
-                body {
-                  font-family: Arial, sans-serif;
-                  margin: 0;
-                  padding: 0;
-                  background-color: #000000;
-                  color: #00000;
-                }
-                .container {
-                  width: 90%;
-                  margin: auto;
-                  max-width: 600px;
-                  border-radius: 10px;
-                  padding: 20px;
-                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                }
-                h2 {
-                  color: #ffffff;
-                }
-                p {
-                  margin: 0 0 10px;
-                  color: #ffffff;
-                }
-                .footer {
-                  text-align: center;
-                  margin-top: 20px;
-                  padding-top: 20px;
-                  border-top: 1px solid #ffffff;
-                }
-                .footer a {
-                  color: #ffffff;
-                  text-decoration: none;
-                  margin: 0 10px;
-                }
-                .footer p {
-                  margin: 5px 0;
-                }
-                .sponsor {
-                  margin-top: 20px;
-                  color: #ffffff;
-                  font-weight: bold;
-                }
-              </style>
-            </head>
-            <body>
-              <div class="container">
-                <h2>Dear ${data.firstName},</h2>
-                <p>Thank you for registering and completing your payment for TEDxCCET. We're thrilled to have you join us for this exciting event!</p>
-                <p>You can find your event tickets and additional details in your profile on our registration platform. Simply log in to your account and navigate to the "My Tickets" section to access and download your tickets.</p>
-                <p>Should you have any questions or require further assistance, please do not hesitate to reach out to us at <a href="mailto:tedxsupport@carmelcet.in">tedxsupport@carmelcet.in</a>.</p>
-                <p>We look forward to welcoming you to TEDxCCET!</p>
-                <div class="footer">
-                  <p>For support, contact us at <a href="mailto:tedxsupport@carmelcet.in">tedxsupport@carmelcet.in</a></p>
-                  <p>Follow us on social media:</p>
-                  <p>
-                    <a href="https://facebook.com/tedxccet" target="_blank">Facebook</a> |
-                    <a href="https://twitter.com/tedxccet" target="_blank">Twitter</a> |
-                    <a href="https://instagram.com/tedxccet" target="_blank">Instagram</a>
-                  </p>
-                  <p class="sponsor">Sponsored by: [Your Sponsors]</p>
-                </div>
-              </div>
-            </body>
-            </html>
-          `,
+          subject: "TEDxCCET Ticket",
+          text: `${data.firstName} ${data.lastName} ${data.organisation}`,
         };
-
 
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
@@ -156,7 +89,7 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
             console.log("Email sent: " + info.response);
           }
         });
-      }
+      });
 
       return new NextResponse(JSON.stringify({ message: "Save successful" }), {
         status: 200,
