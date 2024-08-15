6915b59e58ddc8449ad856ba931c0551fb2a07f7
Joeljoy1237 <joeljoy1237@gmail.com>
2024-08-15 22:03:40 +0530

    feat: early bird

---
+++ src/app/api/ticket/status/route.js
@@ -0,0 +1,17 @@
+import Ticket from "@models/Ticket";
+import { connectToDB } from "@utils/database";
+
+export const GET = async () => {
+  try {
+    await connectToDB();
+    const ticket = await Ticket.findOne({ totalTicket: 100 });
+    return new Response(JSON.stringify({ value: ticket.ticketSold }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ message: "Internal server Error" }), {
+      status: 500,
+    });
+  }
+};
